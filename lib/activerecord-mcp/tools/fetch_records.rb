d5ea1b8cf681e582b769c18f31165aa1da093a74
dak2 <dawt2h@gmail.com>
2025-05-27 09:10:48 +0900

    wip

--- lib/activerecord-mcp/tools/fetch_records.rb
+++ lib/activerecord-mcp/tools/fetch_records.rb
@@ -9,28 +9,29 @@ module ActiveRecordMcp
 
       arguments do
         optional(:model_name).filled(:string).description("Model name to fetch records for (e.g., 'users', 'products'). Use snake_case, plural form. If omitted, returns complete database schema.")
-        optional(:query).filled(:string).description("Query to fetch records for (e.g., 'find all users created today', 'get the user with email john@example.com'). If omitted, returns all records.")
       end
 
-      def call(model_name:, query:)
-        command = "bin/rails runner 'puts ActiveRecord::Base.connection.select_all(`SELECT * FROM #{model_name}`)'"
-        stdout_str, stderr_str, status = Open3.capture3(subprocess_env, command, chdir: "/Users/dak2/Dev/Sources/rails-sandbox")
-
-        if status.success?
-          return stdout_str
-        else
-          return stderr_str
+      def call(model_name:)
+        # command = `bin/rails runner 'puts ActiveRecord::Base.connection.select_all("SELECT * FROM #{model_name}")`
+        command = `bundle exec rails runner 'puts {"id": 1, "name": "dak2"}`
+        output = Dir.chdir(projects_root_file_path) do
+          stdout_str, stderr_str, status = Open3.capture3(subprocess_env, command)
+          if status.success?
+            return stdout_str
+          else
+            return stderr_str
+          end
         end
       end
 
       private
 
       def projects_root_file_path
-        @projects_root_file_path ||= ActiveRecordMcp.configuration.projects_root_file_path
+        @projects_root_file_path ||= File.expand_path("/Users/dak2/Dev/Sources/rails-sandbox")
       end
 
       def subprocess_env
-        subprocess_env = ENV.to_h.merge(Bundler.original_env).merge(
+        ENV.to_h.merge(Bundler.original_env).merge(
           "BUNDLE_GEMFILE" => File.join(projects_root_file_path, "Gemfile")
         )
       end

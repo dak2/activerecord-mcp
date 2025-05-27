33b1ca0bb22c6a03160ae795e997cc51afc7e6a0
dak2 <dawt2h@gmail.com>
2025-05-28 00:02:54 +0900

    output records in another process

--- lib/activerecord-mcp/tools/fetch_records.rb
+++ lib/activerecord-mcp/tools/fetch_records.rb
@@ -12,15 +12,11 @@ module ActiveRecordMcp
       end
 
       def call(model_name:)
-        # command = `bin/rails runner 'puts ActiveRecord::Base.connection.select_all("SELECT * FROM #{model_name}")`
-        command = `bundle exec rails runner 'puts {"id": 1, "name": "dak2"}`
-        output = Dir.chdir(projects_root_file_path) do
-          stdout_str, stderr_str, status = Open3.capture3(subprocess_env, command)
-          if status.success?
-            return stdout_str
-          else
-            return stderr_str
-          end
+        stdout_str, stderr_str, status = Open3.capture3(*capture3_args_for(dir: projects_root_file_path))
+        if status.success?
+          return stdout_str
+        else
+          return stderr_str
         end
       end
 
@@ -30,10 +26,8 @@ module ActiveRecordMcp
         @projects_root_file_path ||= File.expand_path("/Users/dak2/Dev/Sources/rails-sandbox")
       end
 
-      def subprocess_env
-        ENV.to_h.merge(Bundler.original_env).merge(
-          "BUNDLE_GEMFILE" => File.join(projects_root_file_path, "Gemfile")
-        )
+      def capture3_args_for(dir: projects_root_file_path)
+        ["ruby", "-e", "require './config/environment'; puts Post.all.inspect", { chdir: dir }]
       end
     end
   end
